ca494a089d299f886106fdd8f2fd37d194348c03
maqa013 <125148102+maqa013@users.noreply.github.com>
2023-10-03 20:20:54 +0400

    Add files via upload

---
+++ jqanimation.js
@@ -0,0 +1,84 @@
+
+// 1)
+
+//  $('.block1').click(function(){
+//     $(this).css('background','red')
+//  })
+
+// 2)
+// $('.block1').first(function(){
+//      $(this).css('background','green')
+//  })
+
+// 3)
+//  $('.block1').last(function(){
+//     $(this).css('background','green')
+// })
+
+
+//  4)
+//   $('.hide').click(function(){
+//        $('.block').append('<div class="block1" style=" "background-color:blue","height: 50px", "width:50px","margin:3px"></div>')
+//    })
+
+
+
+//  5)
+//   $('.hide').click(function(){
+//        $('.block').prepend('<div class="block1" style=" "background-color:blue","height: 50px", "width:50px","margin:10px"></div>')
+//   })
+
+
+//  4)
+//   $('.hide').click(function(){
+//        $('.block').appendTo('<div class="block1" style=" "background-color:blue","height: 50px", "width:50px","margin:3px"></div>')
+//    })
+
+
+
+//  5)
+//   $('.hide').click(function(){
+//        $('.block').prependTo('<div class="block1" style=" "background-color:blue","height: 50px", "width:50px","margin:10px"></div>')
+//   })
+
+// 6)
+
+// $('.hide').click(function(){
+//     $('.block').before('<div class="block1" style=" "background-color:blue"></div>')
+// })
+
+// 7)
+
+// $('.hide').click(function(){
+//     $('.block').after('<div class="block1" style=" "background:blue"></div>')
+// })
+
+// 8)
+// $('.hide').click(function(){
+//     $('.block').remove()
+// })
+
+
+// 9)
+// $('.hide').click(function(){
+//     $('.block').empty()
+// })
+
+
+// 10)
+//  $('.hide').click(function(){
+//       $('.block1').height($('input').val())
+//       console.log(      $('.block1').height($('input').val()))
+//   })
+
+// 11)
+//  $('.hide').click(function(){
+//     $('.block1').width($('input').val())
+// })
+
+
+
+
+
+
+
